60e8d9c0d952046d04cb447bf36760fedbe4907b
giuliaproserpio <giu.proserpio@gmail.com>
2019-10-29 22:08:26 +0100

    giuliaproserpio

--- sketch.js
+++ sketch.js
@@ -1,11 +1,24 @@
-function preload(){
-  // put preload code here
+
+//this excercise is basically the same of the video tutorial + the color wheel and a input type text to collect the color name
+//tried to change values and math function
+// hadn't really understood the previous lesson... anyway i tried to remake this example!
+//I had tried with the radio button  but it couldn't switch like the slider
+var sliders = []; //array of sliders
+var angle = 0;
+function setup(){
+noCanvas();
+  for (var i = 0; i < windowWidth/5; i++){
+    sliders[i] = createSlider(0, 255, 50);
+}
 }
 
-function setup() {
-  // put setup code here
-}
+function draw(){
+var offset = 0;
+  for (var i = 0; i < sliders.length; i++){
+var x = map(tan(angle+offset), -1, 1, 0, 255);
+  sliders[i].value(x);
+  offset += 0.05;
 
-function draw() {
-  // put drawing code here
+}
+  angle += 0.1;
 }
